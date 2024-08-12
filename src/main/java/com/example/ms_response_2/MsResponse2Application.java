0be724f790f59274087baf2db41baac1fdce4211
cabellodev <72994020+cabellodev@users.noreply.github.com>
2024-08-12 13:29:05 -0400

    Update MsResponse2Application.java

--- src/main/java/com/example/ms_response_2/MsResponse2Application.java
+++ src/main/java/com/example/ms_response_2/MsResponse2Application.java
@@ -1,14 +1,13 @@
 package com.example.ms_response_2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+
 
 @EnableFeignClients
-@ImportAutoConfiguration({FeignAutoConfiguration.class})
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 
 public class MsResponse2Application {
